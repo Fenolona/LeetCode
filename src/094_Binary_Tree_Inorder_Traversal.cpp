8118a3aac17b830e59e8f8a487520993d8daeddd
Fenolona <qlduzhlin@gmail.com>
2015-10-20 18:38:14 +0800

    add iteration version to problem 94

--- src/094_Binary_Tree_Inorder_Traversal.cpp
+++ src/094_Binary_Tree_Inorder_Traversal.cpp
@@ -4,8 +4,9 @@
  * @url             https://leetcode.com/problems/binary-tree-inorder-traversal/
  * @timeComlexity   O(n)
  * @spaceComplexity O(n)
- * @strategy        left child -> root -> right child
+ * @strategy        left child -> root -> right child, recursive
  * @status          Accepted, defeated 12.90% cpp submissions.
+ * @version         recursive
  * @time            10:11, Oct 20 2015
  */
 
@@ -21,14 +22,58 @@
 class Solution {
 public:
     void inorder(TreeNode* root, vector<int>& vec) {
-        if(root->left != NULL) inorder(root->left, vec);
+        if(root == NULL) return;
+        inorder(root->left, vec);
         vec.push_back(root->val);
-        if(root->right != NULL) inorder(root->right, vec);
+        inorder(root->right, vec);
     }
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> vec;
-        if(root == NULL) return vec;
         inorder(root, vec);
+        return vec;
+    }
+};
+
+
+/**
+ * @author          johnsondu
+ * @problem         Binary Tree Inorder Traversal
+ * @url             https://leetcode.com/problems/binary-tree-inorder-traversal/
+ * @timeComlexity   O(n)
+ * @spaceComplexity O(n)
+ * @strategy        left child -> root -> right child, stack to store
+ * @status          Accepted, defeated 12.90% cpp submissions.
+ * @version         iteration
+ * @time            18:36, Oct 20 2015
+ */
+ 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> vec;
+        if(root == NULL) return vec;
+        
+        stack<TreeNode*> st;
+        TreeNode *cur = root;
+        while(!st.empty() || cur != NULL) {
+            while(cur != NULL) {
+                st.push(cur);
+                cur = cur->left;
+            }
+            cur = st.top();
+            st.pop();
+            vec.push_back(cur->val);
+            cur = cur->right;
+        }
         return vec;
     }
 };
